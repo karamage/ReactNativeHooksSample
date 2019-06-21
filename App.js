4bd7fdada1bf59fec0479ab018e103f56181ac2b
karamage <karamage.umauma@gmail.com>
2019-06-21 17:11:22 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, Button, StyleSheet, Text, View} from 'react-native';
+
+const { useState } = React;
+
+const UseStateSample = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View>
+      <Button title={"-"} onPress={() => setCount(count - 1)}></Button>
+      <Text>{count}</Text>
+      <Button title={"+"} onPress={() => setCount(count + 1)}></Button>
+    </View>
+  );
+};
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <UseStateSample/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
